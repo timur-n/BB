6d23b69fb301ad16a80fb96092919c906b81a64d
Tim <timur.n@gmail.com>
2015-09-25 00:47:27 +0100

    Adding multi-bookie and multi-lay data support

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -22,27 +22,74 @@ angular.module('BBApp', [])
 		$scope.data = 0;
 		$scope.events = [];
 		$scope.betfair = createBetfair();
+		$scope.knownBookies = ['Bet 365', 'Sky Bet', 'Ladbrokes'];
 
-		$scope.indexById = function(id) {
+		$scope.indexBy = function(callback) {
 			for (var i = 0; i < $scope.events.length; i += 1) {
-				if ($scope.events[i].id === id) {
+				if (callback($scope.events[i])) {
 					return i;
 				}
 			}
 			return -1;
 		};
 
+		$scope.indexById = function(id) {
+			return $scope.indexBy(function(event) {
+				return event.id === id;
+			});
+		};
+
+		$scope.indexBySmarkets = function(smarketsId) {
+			return $scope.indexBy(function(event) {
+				return event.smarketsUrl === smarketsId;
+			});
+		};
+
+		$scope.indexByBetfair = function(betfairId) {
+			return $scope.indexBy(function(event) {
+				return event.betfairId === betfairId;
+			});
+		};
+
+		function updateSmarkets(event, tabData) {
+			event.smarkets = tabData.data;
+		}
+
+		function updateBookies(event, tabData) {
+			event.data = tabData.data;
+			for (var i = 0; i < $scope.events.length; i += 1) {
+				if (callback($scope.events[i])) {
+					return i;
+				}
+			}
+			event.name = tabData.url;
+		}
+
 		$scope.updateData = function(tabData) {
-			var i = $scope.indexById(tabData.id);
+			var i,
+				isSmarkets = tabData.url.match('smarkets');
+			if (isSmarkets) {
+				i = $scope.indexBySmarkets(tabData.url);
+			} else {
+				i = $scope.indexById(tabData.id);
+			}
 			var event;
-			if (i < 0) {
+			if (i >= 0) {
+				event = $scope.events[i];
+			} else if (!isSmarkets) {
 				event = {id: tabData.id};
 				$scope.events.push(event);
 			} else {
-				event = $scope.events[i];
+				$log.debug('Unknown Smarkets tab data', tabData.url);
 			}
-			event.data = tabData.data;
-			event.name = tabData.url;
+
+			if (event) {
+				if (isSmarkets) {
+					updateSmarkets(event, tabData);
+				} else {
+					updateBookies(event, tabData);
+				}
+			}
 			$scope.$apply();
 		};
 
